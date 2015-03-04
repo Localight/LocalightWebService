bb2d627b81e907c9aa3157bbc2663c860853ff92
BMarcelus <bmdizon@sbcglobal.net>
2015-03-03 16:23:35 -0800

    Regular Expression matches
    added matches with regular expression to the merchant model and updated
    the model to match the iPhone Ambassador app

--- app/models/merchant.server.model.js
+++ app/models/merchant.server.model.js
@@ -10,34 +10,78 @@ var mongoose = require('mongoose'),
  * Merchant Schema
  */
 var MerchantSchema = new Schema({
-	name: {
+	first_name: {
 		type: String,
 		default: '',
-		required: 'Please fill Merchant name',
+		required: 'Please fill Merchant first name',
+		trim: true
+
+		//TODO: 
+	},
+	last_name: {
+		type: String,
+		default: '',
+		required: 'Please fill Merchant last name',
 		trim: true
 	},
 	business_name:{
 		type:String,
 		default: '',
-		required: 'Please fill Business Name'
+		required: 'Please fill Business Name',
+		match : [ [a-z]{3,50}, "please fill out business name"]
+		//TODO: all strings should be all lowercase before saving
+	},
+	shortname:{
+		type:String,
+		default: '',
+		required: 'Please fill Business Name',
+		match: [ [a-z]{3,18}, "please fill out a short name between 3 and 18 charcters"]
+	},
+	locationNumber:{
+		type:Number,
+		default '',
+		required: 'Please fill out location number'
 	},
 	ein:{
 		type:String,
 		default: '',
-		required: 'Please fill EIN'
+		required: 'Please fill EIN', //9 digits
+		match: [/d{9}, 'please']
 	},
 	email:{
 		type:String,
 		default: '',
-		required: 'Please fill in Email'
+		required: 'Please fill in Email',
+		match: [/.+\@.+\..+/, 'Please fill a valid email address']
 		//TODO: get regular expression from other file.
 	},
+	
+	//TODO: add an array of types for merchant model (and a front end menu to select out of the 9 options)
+	// type:[
+	// 	{
+	// 		type: String,
+	// 		default: '',
+	// 	}
+	// 	required: 'Please select type',
+	// ],
 	phoneNumber:{
 		type:String,
 		default: '',
-		required:'Please fill in Phone Number'
+		required:'Please fill in Phone Number',
+		match:[/d{10}, 'Please fill in a valid Phone Number (10 digits)'];
 		//could be number or could be string, get regular expression from other file.
+		//TODO: make sure that the phonenumber has no spaces or hyphens
+		//phone number of whoever is responsible for the bank account
 	},
+	locationPhoneNumber:{
+		type:String,
+		default: '',
+		required:'Please fill in Phone Number',
+		match:[/d{10}, 'Please fill in a valid Phone Number (10 digits)'];
+		//could be number or could be string, get regular expression from other file.
+		//TODO: make sure that the phonenumber has no spaces or hyphens
+	},
+
 	businessAddress:{
 		line1:{
 			type:String,
@@ -64,6 +108,7 @@ var MerchantSchema = new Schema({
 			//krkekquired:'Please fill in Postal code'
 		}
 	},
+
 	/**
 	* BANKING INFORMATION
 	* Default set to empty so they can never actually be filled and put into the database.
@@ -71,10 +116,15 @@ var MerchantSchema = new Schema({
 	accountNumber:{
 		type:String,
 		default: ''
+		
+		//never save account number
+		//4 17
 	},
 	routingNumber:{
 		type:String,
 		default: '',
+		match:[/d{9}, 'Please fill in a valid routing number'];
+		//9 digits
 	},
 	/**
 	* BALANCED API STUFF
