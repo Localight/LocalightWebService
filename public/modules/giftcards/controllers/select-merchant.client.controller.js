cb7a46f77f07922f2b77a1df505e0573e60a29ac
Julian Poyourow <julianpoyo@gmail.com>
2015-06-26 08:19:40 -0700

    Populated temporary merchants array

--- public/modules/giftcards/controllers/select-merchant.client.controller.js
+++ public/modules/giftcards/controllers/select-merchant.client.controller.js
@@ -2,7 +2,24 @@
 
 angular.module('giftcards').controller('SelectMerchantController', ['$scope',
 	function($scope) {
-		// Controller Logic
-		// ...
+		$scope.merchants = [{
+			name: "Goldies On 4th",
+			address: "2106 E 4th St, Long Beach, CA"
+		},{
+			name: "Aji Peruvian Cuisine",
+			address: "2308 E 4th St, Long Beach, CA"
+		},{
+			name: "P3 Artisan Pizza",
+			address: "2306 E 4th St, Long Beach, CA"
+		},{
+			name: "The Social List",
+			address: "2105 E 4th St, Long Beach, CA"
+		},{
+			name: "Lola's",
+			address: "2030 E 4th St, Long Beach, CA"
+		},{
+			name: "Portfolio's Coffee",
+			address: "2300 E 4th St, Long Beach, CA"
+		}]
 	}
 ]);
