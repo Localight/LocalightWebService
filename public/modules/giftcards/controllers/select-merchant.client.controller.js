739bb76e1b04fe34c0033c4c038081e594922aba
Aaron <torch2424@hotmail.com>
2015-06-26 10:17:33 -0700

    did some reorganization of the flat css, and edited our testing javascript

--- public/modules/giftcards/controllers/select-merchant.client.controller.js
+++ public/modules/giftcards/controllers/select-merchant.client.controller.js
@@ -2,26 +2,28 @@
 
 angular.module('giftcards').controller('SelectMerchantController', ['$scope', '$window',
 	function($scope, $window) {
-
-		$scope.init = function(){
-			document.getElementsByClassName(".container").style.padding = "0px";
-		}
 		$scope.merchants = [{
+			area: "4th Street Retro Row",
 			name: "Goldies On 4th",
 			address: "2106 E 4th St, Long Beach, CA"
 		},{
+			area: "4th Street Retro Row",
 			name: "Aji Peruvian Cuisine",
 			address: "2308 E 4th St, Long Beach, CA"
 		},{
+			area: "4th Street Retro Row",
 			name: "P3 Artisan Pizza",
 			address: "2306 E 4th St, Long Beach, CA"
 		},{
+			area: "4th Street Retro Row",
 			name: "The Social List",
 			address: "2105 E 4th St, Long Beach, CA"
 		},{
+			area: "4th Street Retro Row",
 			name: "Lola's",
 			address: "2030 E 4th St, Long Beach, CA"
 		},{
+			area: "4th Street Retro Row",
 			name: "Portfolio's Coffee",
 			address: "2300 E 4th St, Long Beach, CA"
 		}]
