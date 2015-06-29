01be515c6d9e8a82f448ce821f96cc1632e1f978
Aaron <torch2424@hotmail.com>
2015-06-29 12:46:35 -0700

    Added some controller stuff for our testing, and added a bit more styling

--- public/modules/giftcards/controllers/select-merchant.client.controller.js
+++ public/modules/giftcards/controllers/select-merchant.client.controller.js
@@ -2,6 +2,11 @@
 
 angular.module('giftcards').controller('SelectMerchantController', ['$scope', '$window',
 	function($scope, $window) {
+
+		//Initialize scope.giftcards
+		$scope.giftcards = null;
+
+		
 		$scope.merchants = [{
 			area: "4th Street Retro Row",
 			name: "Goldies On 4th",
@@ -27,5 +32,48 @@ angular.module('giftcards').controller('SelectMerchantController', ['$scope', '$
 			name: "Portfolio's Coffee",
 			address: "2300 E 4th St, Long Beach, CA"
 		}]
+
+
+		// Find a list of Giftcards
+		$scope.find = function() {
+			//$scope.giftcards = Giftcards.query();
+
+			//FOr testing, hardcoding scope giftcards
+			$scope.giftcards =
+			[
+				{
+					to: "John",
+					amt: "100",
+					mobileNumberOfRecipient: "5625555555",
+					merchant: "xxxxx",
+					from: 'Tony',
+					message: "hi",
+					districtNumber: 'number',
+					occasionMessage: "Variety is the spice of life. So I'm giving you the gift of choice!"
+				},
+				{
+					to: "John",
+					amt: "100",
+					mobileNumberOfRecipient: "5625555555",
+					merchant: "xxxxx",
+					from: 'Frank',
+					message: "hi",
+					districtNumber: 'number'
+				}
+			]
+		}
+
+		$scope.totalValue = function()
+		{
+			//Get the total value of all the giftcards
+			var total = 0;
+			for(var i = 0; i < $scope.giftcards.length; ++i)
+			{
+				total = total + parseInt($scope.giftcards[i].amt, 10);
+			}
+
+			//Return the total value as a formatted string
+			return "$" + total;
+		}
 	}
 ]);
