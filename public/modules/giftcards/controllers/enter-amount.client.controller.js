045b2a992c9fbaeb873afc4d7bfb9d5d8c077be1
Aaron <torch2424@hotmail.com>
2015-06-30 11:24:50 -0700

    Finally added most of the functionality to the buttons

--- public/modules/giftcards/controllers/enter-amount.client.controller.js
+++ public/modules/giftcards/controllers/enter-amount.client.controller.js
@@ -6,8 +6,17 @@ angular.module('giftcards').controller('EnterAmountController', ['$scope',
 		//Initialize scope.giftcards
 		$scope.giftcards = null;
 
+		//Variable if a button is clicked
+		$scope.clicked = false;
+
 		//Amoutn entered
 		$scope.amount = 0.00;
+
+		//Our stack of digits entered
+		$scope.digitStack = [];
+
+		//Our size of our stack
+		$scope.stackSize = 0;
 
 		//Our variable for which button is selected
 		$scope.pressed = -1;
@@ -57,7 +66,48 @@ angular.module('giftcards').controller('EnterAmountController', ['$scope',
 		//Function to switch the value of pressed
 		$scope.pressButton = function (i)
 		{
+			//Clicked is now true
+			$scope.clicked = true;
+
 			$scope.pressed = i;
+
+			//Ignore values that are negative one, since thye simply disable our selectors
+			if(i != -1)
+			{
+				//Add to our amount from right to left, so just concatanate to the string
+				//push i onto the queue
+				$scope.digitStack.push(i);
+
+				//Increase stack size
+				$scope.stackSize++;
+
+				//create a temp stack we can peek
+				var tempStack = $scope.digitStack.slice();
+
+				//Reset scope amount
+				$scope.amount = 0;
+
+				//Loop to put in our places
+				for(i = $scope.stackSize; i > 0; --i)
+				{
+					//Get the value
+					var pop = tempStack.pop();
+
+					//add it to the amount by multipling it by ten
+					//by a certain power, -3 for cents
+					var add = pop * Math.pow(10, ($scope.stackSize - i - 2));
+
+					console.log(add);
+
+					//Now add the amount to amount
+					$scope.amount = $scope.amount + add;
+
+					//Now format amount
+				}
+
+
+
+			}
 		}
 
 	}
