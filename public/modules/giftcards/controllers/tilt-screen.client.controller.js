742412673e15973b5de646859c1323f7265378f8
Aaron <torch2424@hotmail.com>
2015-07-01 09:32:36 -0700

    Added a function to get our amount, and displayed it to the user

--- public/modules/giftcards/controllers/tilt-screen.client.controller.js
+++ public/modules/giftcards/controllers/tilt-screen.client.controller.js
@@ -1,8 +1,13 @@
 'use strict';
 
 angular.module('giftcards').controller('TiltScreenController', ['$scope',
-	function($scope) {
-		// Controller Logic
-		// ...
+	function($scope)
+	{
+		//Get the amount we are going to send the server
+		$scope.getAmount = function()
+		{
+			//Replace this with a backend call eventually
+			return (parseInt(1000) / 100).toFixed(2);
+		}
 	}
 ]);
