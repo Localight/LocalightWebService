785b2d65261314d24e697b194bada4d94e3f09a6
James Hall <james.o.hall87@gmail.com>
2015-07-02 15:28:52 -0700

    Changed the route to work.

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -65,7 +65,7 @@ module.exports = function(app) {
 	app.route('/auth/reset/:token').post(users.reset);
 
 	app.route('/auth/twilioWebHook/').post(users.twilioWebHook);
-	app.route('/auth/webHookLogin/:token').post(users.twilioHookLogin);
+	app.route('/auth/twilioWebHookLogin/:token').post(users.twilioHookLogin);
 	// Setting up the users authentication api
 	/**
 	 * @api{post} /user Request User Object
