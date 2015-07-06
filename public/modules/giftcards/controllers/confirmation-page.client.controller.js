764132183a1a7a54602d8ec8eab2724d533d1c28
Aaron <torch2424@hotmail.com>
2015-07-06 10:46:26 -0700

    Added timeout functionality of the view

--- public/modules/giftcards/controllers/confirmation-page.client.controller.js
+++ public/modules/giftcards/controllers/confirmation-page.client.controller.js
@@ -1,10 +1,13 @@
 'use strict';
 
-angular.module('giftcards').controller('ConfirmationPageController', ['$scope',
-	function($scope) {
+angular.module('giftcards').controller('ConfirmationPageController', ['$scope', '$timeout', '$location',
+	function($scope, $timeout, $location) {
 
 		//Switch overlay on
 		document.getElementById('darkerOverlay').style.display = "block";
+
+		//Timeout to the next page
+		$timeout(timeoutRedirect, 2000);
 
 		//Get the amount we are going to send the server
 		$scope.getAmount = function()
@@ -18,6 +21,12 @@ angular.module('giftcards').controller('ConfirmationPageController', ['$scope',
 		{
 			//Replace this with a backend call eventually
 			return "Doly's Delectables";
+		}
+
+		//Redirect to the thank you page
+		function timeoutRedirect()
+		{
+    		$location.path("/thankyou");
 		}
 	}
 ]);
