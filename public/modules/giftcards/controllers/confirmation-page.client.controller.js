03c998cc96c6847c60a35ff8b75bd0030e64f486
Aaron <torch2424@hotmail.com>
2015-07-06 10:25:11 -0700

    Added the lower half of the screen, simply need the checkmark and a timeout now

--- public/modules/giftcards/controllers/confirmation-page.client.controller.js
+++ public/modules/giftcards/controllers/confirmation-page.client.controller.js
@@ -2,7 +2,22 @@
 
 angular.module('giftcards').controller('ConfirmationPageController', ['$scope',
 	function($scope) {
-		// Controller Logic
-		// ...
+
+		//Switch overlay on
+		document.getElementById('darkerOverlay').style.display = "block";
+
+		//Get the amount we are going to send the server
+		$scope.getAmount = function()
+		{
+			//Replace this with a backend call eventually
+			return (parseInt(1000) / 100).toFixed(2);
+		}
+
+		//Get the merchant we are going to send the server
+		$scope.getMerchant = function()
+		{
+			//Replace this with a backend call eventually
+			return "Doly's Delectables";
+		}
 	}
 ]);
