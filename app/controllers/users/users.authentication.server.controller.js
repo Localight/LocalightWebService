5930ba3a469c720ebf0ba6e1db4c852c30bb4325
James Hall <greatwolf3d@gmail.com>
2015-07-12 16:22:29 -0700

    well that didn't work

--- app/controllers/users/users.authentication.server.controller.js
+++ app/controllers/users/users.authentication.server.controller.js
@@ -89,7 +89,7 @@ exports.twilioWebHookLogin = function(req, res, next) {
                   message: err
                });
             } else {
-               return res.redirect('#!/giftcards/create').json(user);
+               return res.redirect('#!/giftcards/create');
                // // res.json(user0);
                // res.renderX('index', {// there has to be a way to force it to redirect to antoher page.
             	// 	user: req.user || null,
