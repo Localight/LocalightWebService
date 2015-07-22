62e2004744eb16ee24fc1ded453264e497b1164e
Aaron <torch2424@hotmail.com>
2015-07-22 14:43:14 -0700

    Generated the users service

---
+++ app/scripts/services/users.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name angularLocalightApp.Users
+ * @description
+ * # Users
+ * Service in the angularLocalightApp.
+ */
+angular.module('angularLocalightApp')
+  .service('Users', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
