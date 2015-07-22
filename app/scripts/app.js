9a6db94a99404c801eedb6f2012436c916e9bec0
Aaron <torch2424@hotmail.com>
2015-07-22 11:12:23 -0700

    Generated a default Up-to-Date Angular project, and completely ported Stripe, The create giftcard page, index, list giftcards page, css, and other various things. This is a great point for rolling back to with grunt and everything working fine

---
+++ app/scripts/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularLocalightApp
+ * @description
+ * # angularLocalightApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularLocalightApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/giftcards/create/:token', {
+        templateUrl: 'views/creategiftcard.html',
+        controller: 'CreategiftcardCtrl',
+        controllerAs: 'createGiftcard'
+      })
+      .when('/giftcards', {
+        templateUrl: 'views/listgiftcards.html',
+        controller: 'ListgiftcardsCtrl',
+        controllerAs: 'listGiftcards'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
