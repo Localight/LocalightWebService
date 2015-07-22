7cac42b15cb1fe92969450cd5f45f7264b23f410
Aaron <torch2424@hotmail.com>
2015-07-22 14:57:38 -0700

    Added join and login to the service

--- app/scripts/services/users.js
+++ app/scripts/services/users.js
@@ -8,6 +8,29 @@
  * Service in the angularLocalightApp.
  */
 angular.module('angularLocalightApp')
-  .service('Users', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+.factory('Login', ['$resource', function($resource) {
+
+return $resource( '/users/login',
+    { }, {
+        submit: {
+            method: 'POST',
+            params: {},
+            isArray: false
+        }
+
+    } );
+}]);
+
+angular.module('angularLocalightApp')
+.factory('Join', ['$resource', function($resource) {
+
+return $resource( '/users/join',
+    { }, {
+        submit: {
+            method: 'POST',
+            params: {},
+            isArray: false
+        }
+
+    } );
+}]);
