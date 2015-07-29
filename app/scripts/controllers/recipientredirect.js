29496a70fdb7cb90d523ffc0693004c005c678df
Aaron <torch2424@hotmail.com>
2015-07-29 14:22:01 -0700

    Finished the functionality of the redirect page

--- app/scripts/controllers/recipientredirect.js
+++ app/scripts/controllers/recipientredirect.js
@@ -8,12 +8,27 @@
  * Controller of the angularLocalightApp
  */
 angular.module('angularLocalightApp')
-  .controller('RecipientredirectCtrl', function () {
+  .controller('RecipientredirectCtrl', function ($location, $routeParams, $cookies, $timeout) {
 
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-    
+
+    //Get the session token and save it
+    var sessionToken = $routeParams.token;
+
+    $cookies.put("sessionToken", sessionToken);
+
+    //Timeout to the next page
+    $timeout(timeoutRedirect, 2000);
+
+    //Redirect to the thank you page
+    function timeoutRedirect()
+    {
+        $location.path("/giftcards");
+    }
+
+
   });
