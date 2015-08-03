df37a772535ef84e45c2008847871696f8c2c000
Aaron <torch2424@hotmail.com>
2015-08-03 16:07:20 -0700

    Generated the locations service!

---
+++ app/scripts/services/locations.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name angularLocalightApp.locations
+ * @description
+ * # locations
+ * Service in the angularLocalightApp.
+ */
+angular.module('angularLocalightApp')
+  .service('locations', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
