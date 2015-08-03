0d5b06089cc803e4bf9f4bee65f15a59af071b22
Aaron <torch2424@hotmail.com>
2015-08-03 13:47:29 -0700

    Used the angular window instead of the javascript window

--- app/scripts/controllers/creategiftcard.js
+++ app/scripts/controllers/creategiftcard.js
@@ -12,7 +12,7 @@ angular.module('angularLocalightApp')
   $log, $q, $cookies, OccasionService, Users, Join, Giftcards) {
 
     //Check for device orientation
-    window.addEventListener("orientationchange", function() {
+    $window.addEventListener("orientationchange", function() {
         if($window.orientation == -90 || $window.orientation == 90)
         {
             alert("Please disable device rotation, this app is meant to be used in portrait mode. You could risk spending a giftcard incorrectly, or losing your data.");
