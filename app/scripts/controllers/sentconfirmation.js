0f534235945ea995748084b067f3108acb11e68f
Aaron <torch2424@hotmail.com>
2015-08-03 09:44:02 -0700

    Finished the controller logic, and tested working

--- app/scripts/controllers/sentconfirmation.js
+++ app/scripts/controllers/sentconfirmation.js
@@ -8,12 +8,23 @@
  * Controller of the angularLocalightApp
  */
 angular.module('angularLocalightApp')
-  .controller('SentconfirmationCtrl', function () {
+  .controller('SentconfirmationCtrl', function ($scope, $cookies) {
 
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-    
+
+    $scope.getInit = function()
+    {
+        //Place our cookies in scope variables and then remove the cookies
+        $scope.phoneNum = $cookies.get("phone");
+        $scope.email = $cookies.get("email");
+
+        //Remove the cookies
+        $cookies.remove("phone");
+        $cookies.remove("email");
+    }
+
   });
