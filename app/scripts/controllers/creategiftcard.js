13b52623584a9cdaa32f246ae842b2dac626037f
Aaron <torch2424@hotmail.com>
2015-08-03 13:45:29 -0700

    Added an alert for device rotation on the create giftcard page if turned to landscape

--- app/scripts/controllers/creategiftcard.js
+++ app/scripts/controllers/creategiftcard.js
@@ -11,6 +11,14 @@ angular.module('angularLocalightApp')
   .controller('CreategiftcardCtrl', function ($scope, $http, $routeParams, $location, $window, $timeout,
   $log, $q, $cookies, OccasionService, Users, Join, Giftcards) {
 
+    //Check for device orientation
+    window.addEventListener("orientationchange", function() {
+        if($window.orientation == -90 || $window.orientation == 90)
+        {
+            alert("Please disable device rotation, this app is meant to be used in portrait mode. You could risk spending a giftcard incorrectly, or losing your data.");
+        }
+    }, false);
+
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
