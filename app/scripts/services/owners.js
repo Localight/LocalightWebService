d0bf399c51c6bf4c994a17a7b32e71a26fe6e8d8
Aaron <torch2424@hotmail.com>
2015-08-05 10:29:41 -0700

    Generated the owners Service

---
+++ app/scripts/services/owners.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name angularLocalightApp.Owners
+ * @description
+ * # Owners
+ * Service in the angularLocalightApp.
+ */
+angular.module('angularLocalightApp')
+  .service('Owners', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
