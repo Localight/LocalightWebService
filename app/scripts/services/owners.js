119bf1dd68c88196316ec092599a0f65ec915033
Aaron <torch2424@hotmail.com>
2015-08-05 11:11:06 -0700

    Added the getting ofan owner in the service

--- app/scripts/services/owners.js
+++ app/scripts/services/owners.js
@@ -8,6 +8,20 @@
  * Service in the angularLocalightApp.
  */
  angular.module('angularLocalightApp')
+ .factory('Owners', ['$resource', function($resource) {
+
+ return $resource( window.location.protocol + "//" + window.location.hostname + ':3000/owners',
+     { }, {
+         get: {
+             method: 'GET',
+             params: {},
+             isArray: false
+         }
+
+     } );
+ }]);
+
+ angular.module('angularLocalightApp')
  .factory('JoinOwner', ['$resource', function($resource) {
 
  return $resource( window.location.protocol + "//" + window.location.hostname + ':3000/owners/join',
