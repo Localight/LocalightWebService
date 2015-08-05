6250bff5297c06f389a72fc4758e4fb801490293
Aaron <torch2424@hotmail.com>
2015-08-05 11:26:08 -0700

    Got getting locations and displaying them in an ng repeat

--- app/scripts/controllers/mainpanel.js
+++ app/scripts/controllers/mainpanel.js
@@ -8,7 +8,7 @@
  * Controller of the angularLocalightApp
  */
 angular.module('angularLocalightApp')
-  .controller('MainpanelCtrl', function ($scope, $cookies, $location, LoginOwner) {
+  .controller('MainpanelCtrl', function ($scope, $cookies, $location, Owners, Locations) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -16,8 +16,54 @@ angular.module('angularLocalightApp')
     ];
 
     //Grab our session token
-    $cookies.get("sessionToken");
+    var sessionToken = $cookies.get("sessionToken");
 
-    
+    //Get our owner Info
+    $scope.getOwner = function() {
+
+        //First set up some JSON for the session token
+        var getJson = {
+           "sessionToken" : sessionToken
+        }
+
+        $scope.owner = Owners.get(getJson, function(){
+            //Check for errors
+            if($scope.owner.errorid)
+            {
+                console.log($scope.owner.msg);
+                return;
+            }
+            else
+            {
+                //there was no error continue as normal
+                //Save their session token
+
+            }
+        });
+    }
+
+    //get the locations
+    $scope.getLocations = function() {
+        //Json to send to the backend
+        var locationJson = {
+            "sessionToken" : $scope.sessionToken
+        }
+
+        $scope.locations = Locations.get(locationJson, function()
+        {
+            //Check for errors
+            if($scope.locations.errorid)
+            {
+                console.log($scope.locations.errorid + ": " + $scope.locations.msg);
+                return;
+            }
+            else {
+                //there was no error continue as normal
+                //Stop any loading bars or things here
+                console.log("Whats up?");
+            }
+        });
+    }
+
 
   });
