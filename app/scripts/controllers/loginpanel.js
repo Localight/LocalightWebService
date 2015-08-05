4ce1372663948ed3c83549681d874ca0e756fed7
Aaron <torch2424@hotmail.com>
2015-08-05 11:03:48 -0700

    Finished the login page

--- app/scripts/controllers/loginpanel.js
+++ app/scripts/controllers/loginpanel.js
@@ -8,10 +8,43 @@
  * Controller of the angularLocalightApp
  */
 angular.module('angularLocalightApp')
-  .controller('LoginpanelCtrl', function () {
+  .controller('LoginpanelCtrl', function ($scope, $cookies, $location, LoginOwner) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    //no errors
+    $scope.submitError;
+
+    //Sign up our owner!
+    $scope.signUp = function() {
+
+        //First set up some JSON for the session token
+        var postJson = {
+           "email" : $scope.email,
+           "password" : $scope.password
+        }
+
+        $scope.owner = LoginOwner.submit(postJson, function(){
+            //Check for errors
+            if($scope.owner.errorid)
+            {
+                $scope.submitError = true;
+                $scope.theError = $scope.owner.msg;
+                return;
+            }
+            else
+            {
+                //there was no error continue as normal
+                //Save their session token
+                $cookies.put("sessionToken", $scope.owner.token);
+
+                //Finally redirect to the main page
+                $location.path("/panel/main");
+            }
+        });
+
+    }
   });
