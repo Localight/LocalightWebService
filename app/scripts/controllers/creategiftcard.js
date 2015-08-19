a2b63604bd8573dd47fdb99834c00fdbc7db86a0
Aaron <torch2424@hotmail.com>
2015-08-19 08:35:46 -0700

    Added an id to the card div strip. And now we scroll to that, instead of the disappearing code field on show

--- app/scripts/controllers/creategiftcard.js
+++ app/scripts/controllers/creategiftcard.js
@@ -169,6 +169,9 @@ angular.module('angularLocalightApp')
                 frontCard.className = frontCard.className + " flipped";
                 backCard.className = backCard.className + " flipped";
             }, 500);
+
+            //Try and scroll to the card again, in case iPhone pushed it away
+            $scope.scrollToElement("cardCodeStrip");
         };
 
         $scope.setAmount = function(amount) {
