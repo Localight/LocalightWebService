d0cbd531999035d1ed623bd6d714cfb67f0f63a0
Julian Poyourow <julianpoyo@gmail.com>
2015-10-07 15:02:44 -0700

    Preload grunt-ng-constant

--- Gruntfile.js
+++ Gruntfile.js
@@ -473,6 +473,8 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-ng-constant');
+
   grunt.registerTask('serve', 'Compile then start a connect web server', function(target) {
     if (target === 'prod') {
       grunt.task.run([
