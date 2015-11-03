8382d2ea43aa75868516183cf0a67248ff6d692c
Julian Poyourow <julianpoyo@gmail.com>
2015-11-02 18:11:33 -0800

    Background now blurs in

--- app/scripts/controllers/bodybg.js
+++ app/scripts/controllers/bodybg.js
@@ -8,12 +8,17 @@
  * Controller of the angularLocalightApp
  */
 angular.module('angularLocalightApp')
-  .controller('BodyCtrl', function ($scope, $location) {
+  .controller('BodyCtrl', function ($scope, $location, $timeout) {
 
       //If the route is the route passed,
       //it will switch the body ng-class
       $scope.isDark = function(route) {
           return ($location.path().indexOf(route) > -1);
     }
+
+    $timeout(function(){
+        var html = document.getElementsByTagName("html");
+        html[0].className = html[0].className + " bgFade";
+    }, 100);
 
   });
