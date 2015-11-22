ab151d9f528bf8e7f03d58d56399a76b0498f986
Aaron <torch2424@hotmail.com>
2015-11-22 13:17:32 -0800

    I think I got the background completely working on both iPhone and android! :'D

--- app/scripts/controllers/bodyBg.js
+++ app/scripts/controllers/bodyBg.js
@@ -10,6 +10,9 @@
 angular.module('angularLocalightApp')
   .controller('BodyCtrl', function ($scope, $location, $timeout) {
 
+    //Our html tag
+    var html = document.documentElement;
+
     //If the route is the route passed,
     //it will switch the body ng-class
     $scope.isDark = function(route) {
@@ -17,9 +20,9 @@ angular.module('angularLocalightApp')
         //Check to also switch our html color here with dark routes
         //Colors should be kept in sync with colors sass
         if($location.path().indexOf(route) > -1) {
-            document.documentElement.backgroundColor = "#316D6B"
+            html.backgroundColor = "#316D6B"
         }
-        else document.documentElement.backgroundColor = "#4fcfc8"
+        else html.backgroundColor = "#4fcfc8"
 
       return ($location.path().indexOf(route) > -1);
     }
@@ -39,10 +42,14 @@ angular.module('angularLocalightApp')
     };
 
     //Grab the device platform for android/iphone background switching
+    //Checking if the platform contains any iphone in it's platform
+    //if(navigator.userAgent.indexOf("iPhone") < 0) alert(navigator.userAgent);
 
+    //Set the position of the background to be the device height
+    //We want about 60% of the viewport html.clientHeight * .4
+    html.style.backgroundPosition = "100% " + (html.clientHeight * .4375) + "px";
 
     $timeout(function(){
-        var html = document.getElementsByTagName("html");
-        html[0].className = html[0].className + " bgFade";
+        html.className = html.className + " bgFade";
     }, 100);
   });
