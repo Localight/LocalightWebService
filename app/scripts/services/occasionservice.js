99ac3f7e53bf00326c88fb909c92e0060ac6c8fc
Aaron <torch2424@hotmail.com>
2015-12-04 15:24:04 -0800

    Occasions Service
    Found out the create giftcard was using the wrong ids for the occasions
    compared to arrays scattered throughout code, cleaned this up, and now
    simply needs to be nimplemented throughout the app

--- app/scripts/services/occasionservice.js
+++ app/scripts/services/occasionservice.js
@@ -17,6 +17,8 @@
 
 angular.module('angularLocalightApp')
   .service('OccasionService', function(){
+
+  //Our array of occasions
   var occasions = [
       {
         name: 'birthday',
@@ -119,5 +121,29 @@ angular.module('angularLocalightApp')
         text: 'If you want to be loved for who you are, just be yourself.'
       }
     ];
-    return occasions;
+
+
+    //Return our occasions by ID and  or the entire array
+    return {
+
+        //Get all the occasions
+        'getAllOccasions': function() {
+            return occasions;
+        }
+
+        //Return an occasion by it's id
+        'getOccasionsById': function(Id) {
+
+            //Loop through and find the occasions with the specified Id
+            for(var i = 0; i < occasions.lenght; i++)
+            {
+                if(occasions[i].images.iconId == Id) {
+                    return occasions[i];
+                }
+            }
+
+            //If nothing is returned, just return the custom occasion
+            return occasions[9];
+        }
+    }
  });
