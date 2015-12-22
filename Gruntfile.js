b0753f82bd2c7df069c243116de9425c3772ab2c
Aaron <torch2424@hotmail.com>
2015-12-21 16:22:20 -0800

    Create Giftcard And Fixes
    Added the localight logo to the bottom of the create giftcard page, and
    did some additional minor fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -455,7 +455,7 @@ module.exports = function(grunt) {
         },
         constants: {
           ENV: {
-            API_BASE: 'http://jnode.ngrok.kondeo.com:8080'
+            API_BASE: 'http://dev.localight.com:3001'
           }
         }
       },
